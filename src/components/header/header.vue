dfe06c356494497b042f05841c06ce593c1ad0b2
CoderQiQin <296670156@qq.com>
2017-11-13 00:30:11 +0800

    1
    1

---
+++ src/components/header/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    woshi1header
+  </header>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+
+    },
+    created() {
+
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
