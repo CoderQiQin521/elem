5195ee2d8a2eb4102c5410a1822986be1d45ca8a
CoderQiQin521 <296670156@qq.com>
2017-11-17 18:50:40 +0800

    1
    1

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
   	<div class="menu-wrapper" ref="menuWrapper">
   		<ul>
-  			<li v-for="item in goods" class="menu-item">
+  			<li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex === index}">
   				<span class="text border-1px">
   					<span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
   				</span>
@@ -11,7 +11,7 @@
   	</div>
   	<div class="foods-wrapper" ref="foodsWrapper">
   		<ul>
-  			<li v-for="item in goods" class="food-list">
+  			<li v-for="item in goods" class="food-list" ref="foodList">
   				<h1 class="title">{{item.name}}</h1>
   				<ul>
   					<li v-for="food in item.foods" class="food-item border-1px">
@@ -32,7 +32,6 @@
   				</ul>
   			</li>
   		</ul>
-  		右侧
   	</div>
   </div>
 </template>
@@ -49,8 +48,22 @@
   	},
   	data() {
   		return {
-  			goods: []
+  			goods: [],
+  			listHeight: [],
+  			scrollY: 0
   		};
+  	},
+  	computed: {
+  		currentIndex() {
+  			for (let i = 0; i < this.listHeight.length; i++) {
+  				let height1 = this.listHeight[i];
+  				let height2 = this.listHeight[i + 1];
+  				if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+  					return i;
+  				}
+  			}
+  			return 0;
+  		}
   	},
   	created() {
   		this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -60,6 +73,7 @@
   				this.goods = response.data;
   				this.$nextTick(() => {
   					this._initScroll();
+  					this._calculateHeight();
   				});
   			}
   		});
@@ -70,8 +84,22 @@
   				click: true
   			});
   			this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
-  				click: true
+  				click: true,
+  				probeType: 3
   			});
+  			this.foodsScroll.on('scroll', (pos) => {
+  				this.scrollY = Math.abs(Math.round(pos.y));
+  			});
+  		},
+  		_calculateHeight() {
+  			let foodList = this.$refs.foodList;
+  			let height = 0;
+  			this.listHeight.push(height);
+  			for (let i = 0; i < foodList.length; i++) {
+  				let item = foodList[i];
+  				height += item.clientHeight;
+  				this.listHeight.push(height);
+  			}
   		}
   	}
   };
@@ -96,6 +124,14 @@
 				height: 54px
 				width: 56px
 				line-height: 14px
+				&.current
+					position: relative
+					z-index: 10
+					margin-top: -1px
+					font-weight: 700
+					background: #fff
+					.text
+						border-none()
 				.icon
 					display: inline-block
 					vertical-align: top
