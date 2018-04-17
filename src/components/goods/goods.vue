89e0d1cda97441f41a95c5acd8bbf73af58f78f7
CoderQiQin521 <296670156@qq.com>
2018-04-17 14:44:34 +0800

    food

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -14,7 +14,7 @@
   			<li v-for="item in goods" class="food-list" ref="foodList">
   				<h1 class="title">{{item.name}}</h1>
   				<ul>
-  					<li v-for="food in item.foods" class="food-item border-1px">
+  					<li @click="selectFood(food, $event)" v-for="food in item.foods" class="food-item border-1px">
   						<div class="icon">
   							<img width="57" height="57" :src="food.icon"/>
   						</div>
@@ -37,6 +37,7 @@
   		</ul>
   	</div>
   	<shopcart ref="shopcart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+  	<food :food="selectedFood" ref="food"></food>
   </div>
 </template>
 
@@ -44,6 +45,7 @@
 	import BScroll from 'better-scroll';
 	import shopcart from '@/components/shopcart/shopcart';
 	import cartcontrol from '@/components/cartcontrol/cartcontrol';
+	import food from '@/components/food/food';
 
 	const ERR_OK = 0;
   export default {
@@ -56,7 +58,8 @@
   		return {
   			goods: [],
   			listHeight: [],
-  			scrollY: 0
+  			scrollY: 0,
+  			selectedFood: {}
   		};
   	},
   	computed: {
@@ -136,11 +139,19 @@
   			this.$nextTick(() => {
   				this.$refs.shopcart.drop(target);
   			});
+  		},
+  		selectFood(food, $event) {
+  			if (!event._constructed) {
+  				return;
+  			}
+  			this.selectedFood = food;
+  			this.$refs.food.show();
   		}
   	},
   	components: {
   		shopcart,
-  		cartcontrol
+  		cartcontrol,
+  		food
   	}
   };
 </script>
